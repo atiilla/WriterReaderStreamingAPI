d861bef4c1cd31474f174e4c71e61d5e1d6116ff
intec <intec@intec.intecbrussel.be>
2018-09-03 13:16:44 +0200

    Create folder and file type something in file and read it.

--- src/be/intecbrussel/SecureMsg/MessageApp.java
+++ src/be/intecbrussel/SecureMsg/MessageApp.java
@@ -6,7 +6,7 @@ public class MessageApp {
 		//System.out.println("OS: " + System.getProperty("os.name") + ", USER DIRECTORY: " + System.getProperty("user.home"));
 		
 		// "Folder" , "FileName" , "Your Msg" >>> Msg will be encrypted
-		MsgUtils msg = new MsgUtils("Atilla", "Fistik", "Naber Lan");
+		MsgUtils msg = new MsgUtils("Foldername", "FileName", "ContentFileTXT");
 		System.out.println(msg);
 		
 		// Create folder and TxtFile and typing msg into the TxtFile
